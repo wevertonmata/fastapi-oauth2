f1d1477411a86e4b271c125d85a802f63f98a762
wevertonmata <weverton.mata@outlook.com>
2023-03-27 08:45:13 -0300

    api

--- src/api/v1/api.py
+++ src/api/v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from .endpoints import curso
+from .endpoints import artigo, usuario
 
 
 api_router_v1 = APIRouter()
-api_router_v1.include_router(curso.router, tags=['cursos'], prefix="/cursos")
+api_router_v1.include_router(artigo.router, tags=['artigos'], prefix="/artigos")
+api_router_v1.include_router(usuario.router, tags=['usuarios'], prefix="/usuarios")
